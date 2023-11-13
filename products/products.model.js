70c28126cf72b9ebf1b6443e8b38278992e4499e
Fabian Veliz <velizfabianhoracio@gmail.com>
2023-11-13 11:40:47 -0300

    Add mutations to add products and review

--- products/products.model.js
+++ products/products.model.js
@@ -2,12 +2,14 @@ const products = [
     {
         id: 'red-shoe',
         description: 'A red shoe',
-        price: 42.50
+        price: 42.50,
+        reviews: []
     },
     {
         id: 'blue-jean',
         description: 'A blue jean',
-        price: 20.00
+        price: 20.00,
+        reviews: []
     }
 ]
 
@@ -23,8 +25,31 @@ function getProductByID(id) {
     return products.find(p => p.id === id)
 }
 
+function addNewProduct(id, description, price) {
+    const newProduct = {
+        id,
+        description,
+        price,
+        reviews: []
+    }
+    products.push(newProduct)
+    return newProduct
+}
+
+function addNewProductReview(productID, rating, comment) {
+    const product = getProductByID(productID)
+    if (product) {
+        const newReview = { rating, comment }
+        product.reviews.push(newReview)
+        return newReview
+    }
+    return console.error(`Product with id: ${productID} not found!`)
+}
+
 module.exports = {
     getAllProducts,
     getProductsByPrice,
-    getProductByID
+    getProductByID,
+    addNewProduct,
+    addNewProductReview
 }
--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -1,9 +1,13 @@
-const { getAllProducts, getProductsByPrice, getProductByID } = require('./products.model')
+const { getAllProducts, getProductsByPrice, getProductByID, addNewProduct, addNewProductReview } = require('./products.model')
 
 module.exports = {
     Query: {
         products: () => getAllProducts(),
         productsByPrice: (_, { minPrice, maxPrice }) => getProductsByPrice(minPrice, maxPrice),
         product: (_, { id }) => getProductByID(id)
+    },
+    Mutation: {
+        addNewProduct: (_, args) => addNewProduct(args.id, args.description, args.price),
+        addNewProductReview: (_, args) => addNewProductReview(args.id, args.rating, args.comment)
     }
 }
